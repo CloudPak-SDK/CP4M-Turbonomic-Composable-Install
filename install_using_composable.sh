7029e1a098cad5a571abfed5f67537ad9f3ab7df
zach-robinson <62120488+zach-robinson@users.noreply.github.com>
2021-04-06 11:25:47 -0500

    Shorten sleep

--- install_using_composable.sh
+++ install_using_composable.sh
@@ -219,8 +219,8 @@ spec:
                 - "Base64ToString" 
 EOF
 #Wait for API Pod to be ready
-until oc -n ${NS} get xl xl-release >> /dev/null 2>&1; do sleep 5; done
-sleep 60
+until oc -n ${NS} get xl xl-release >>/dev/null 2>&1; do sleep 5; done
+sleep 10
 oc -n ${NS} wait --'for=condition=Ready' pod -l app.kubernetes.io/name=api --timeout 10m
 API_POD=$(oc -n ${NS} get po -l app.kubernetes.io/name=api -o go-template='{{ (index .items 0).metadata.name }}')
 until oc -n ${NS} get secret cs-ca-certificate-secret -o jsonpath={.data."ca\.crt"} | base64 -d > ca.crt 2>>/dev/null; do sleep 5; done 
