c359116e4953a41370d36a273636375b9461da6e
zach-robinson <62120488+zach-robinson@users.noreply.github.com>
2021-04-06 11:33:10 -0500

    Use Keytool bundled in PPA  when needed

--- install_using_composable.sh
+++ install_using_composable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Offer EULA
-which java
+which java >> /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	java -jar LAPApp.jar -l Softcopy -s Status -text_only
 else
@@ -223,8 +223,10 @@ until oc -n ${NS} get xl xl-release >>/dev/null 2>&1; do sleep 5; done
 sleep 10
 oc -n ${NS} wait --'for=condition=Ready' pod -l app.kubernetes.io/name=api --timeout 10m
 API_POD=$(oc -n ${NS} get po -l app.kubernetes.io/name=api -o go-template='{{ (index .items 0).metadata.name }}')
-until oc -n ${NS} get secret cs-ca-certificate-secret -o jsonpath={.data."ca\.crt"} | base64 -d > ca.crt 2>>/dev/null; do sleep 5; done 
-rm -f cacerts && keytool -import -trustcacerts -alias cp -file ca.crt -keystore cacerts -storepass changeit -noprompt
+until oc -n ${NS} get secret cs-ca-certificate-secret -o jsonpath={.data."ca\.crt"} | base64 -d > ca.crt 2>>/dev/null; do sleep 5; done
+KT=keytool
+which keytool >> /dev/null 2>&1 || (tar xzf OpenJDK11U-jre_x64_linux_hotspot_11.0.7_10.tar.gz && KT=jdk-11.0.7+10-jre/bin/keytool)
+rm -f cacerts && ${KT} -import -trustcacerts -alias cp -file ca.crt -keystore cacerts -storepass changeit -noprompt
 oc -n ${NS} cp cacerts ${API_POD}:/home/turbonomic/data
 oc -n ${NS} delete pod ${API_POD}
 oc -n ${NS} wait --'for=condition=Ready' pod -l app.kubernetes.io/name=api --timeout 10m
