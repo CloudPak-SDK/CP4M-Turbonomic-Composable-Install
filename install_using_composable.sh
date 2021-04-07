5d2a4324886de2c9b2e4ec0a518c8f932963945b
zach-robinson <62120488+zach-robinson@users.noreply.github.com>
2021-04-07 09:42:26 -0500

    Delete cacerts before copy-over

--- install_using_composable.sh
+++ install_using_composable.sh
@@ -226,8 +226,8 @@ API_POD=$(oc -n ${NS} get po -l app.kubernetes.io/name=api -o go-template='{{ (i
 until oc -n ${NS} get secret cs-ca-certificate-secret -o jsonpath={.data."ca\.crt"} | base64 -d > ca.crt 2>>/dev/null; do sleep 5; done
 KT=keytool
 which keytool >> /dev/null 2>&1 || tar xzf OpenJDK11U-jre_x64_linux_hotspot_11.0.7_10.tar.gz && KT=jdk-11.0.7+10-jre/bin/keytool
-cp /etc/pki/ca-trust/extracted/java/cacerts .
-rm -f cacerts && ${KT} -import -trustcacerts -alias cp -file ca.crt -keystore cacerts -storepass changeit -noprompt
+rm -f cacerts && cp /etc/pki/ca-trust/extracted/java/cacerts .
+${KT} -import -trustcacerts -alias cp -file ca.crt -keystore cacerts -storepass changeit -noprompt
 oc -n ${NS} cp cacerts ${API_POD}:/home/turbonomic/data
 oc -n ${NS} delete pod ${API_POD}
 oc -n ${NS} wait --'for=condition=Ready' pod -l app.kubernetes.io/name=api --timeout 10m
